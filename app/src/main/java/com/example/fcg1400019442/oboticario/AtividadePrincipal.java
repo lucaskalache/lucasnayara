7482a8b890c9735ea26231c843c424da69a89207
Lucas <lucas_kalache@hotmail.com>
2015-06-15 10:56:20 +0000

    Tarefa 2 completa

--- app/src/main/java/com/example/fcg1400019442/oboticario/AtividadePrincipal.java
+++ app/src/main/java/com/example/fcg1400019442/oboticario/AtividadePrincipal.java
@@ -4,17 +4,51 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 @SuppressWarnings("ALL")
 public class AtividadePrincipal extends ActionBarActivity {
+    private ArrayAdapter<String> mAdaptador;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_atividade_principal);
-    }
 
+        String[] dados = {
+                "7  Tentações Egeo",
+                "Atraia",
+                "Provoque",
+                "Seduza",
+                "Agarre",
+                "Devore",
+                "Desfrute"
+        };
+
+        List<String> oboticario = new ArrayList<>(Arrays.asList(dados));
+
+
+        mAdaptador = new ArrayAdapter<>(
+                getApplicationContext(), // Contexto atual
+                R.layout.item_lista_principal, // Nome do ID do layout
+                R.id.item_texto, // ID do TextView a ser preenchido
+                oboticario);
+
+        ListView listView = (ListView) findViewById(R.id.lista_principal);
+        listView.setAdapter(mAdaptador);
+
+
+
+
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -36,4 +70,6 @@ public class AtividadePrincipal extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
